ca3eec8145e04a84d16c378521a64930f6b54a2f
Juan Alba <juanalba@Juans-MacBook-Pro.local>
2019-05-21 14:02:28 -0300

    Maximum values for day input.

--- containers/dayTrackScreen.js
+++ containers/dayTrackScreen.js
@@ -31,28 +31,34 @@ class DayTrackScreen extends React.Component {
       <View style={generalStyles.container}>
         <DayTrackRow
           description={constants.fruits}
-          propValue={this.props[constants.fruits]}
+          propValue={this.props.dayTrack[constants.fruits]}
           increment={() => { this.addFood(constants.fruits) }}
           decrement={() => {this.subtractFood(constants.fruits) }}
+          disableIncrement = {this.props.dietConfig[constants.fruits + 'MAX'] === this.props.dayTrack[constants.fruits]}
+          disableDecrement = {this.props.dayTrack[constants.fruits] === 0}
         />
         <DayTrackRow
           description={constants.carbo}
-          propValue={this.props[constants.carbo]}
+          propValue={this.props.dayTrack[constants.carbo]}
           increment={() => { this.addFood(constants.carbo) }}
           decrement={() => {this.subtractFood(constants.carbo) }}
+          disableIncrement = {this.props.dietConfig[constants.carbo + 'MAX'] === this.props.dayTrack[constants.carbo]}
+          disableDecrement = {this.props.dayTrack[constants.carbo] === 0}
         />
         <DayTrackRow
           description={constants.snatch}
-          propValue={this.props[constants.snatch]}
+          propValue={this.props.dayTrack[constants.snatch]}
           increment={() => { this.addFood(constants.snatch) }}
           decrement={() => {this.subtractFood(constants.snatch) }}
+          disableIncrement = {this.props.dietConfig[constants.snatch + 'MAX'] === this.props.dayTrack[constants.snatch]}
+          disableDecrement = {this.props.dayTrack[constants.snatch] === 0}
         />
       </View>
     )
   }
 }
 
-const mapStateToProps = (state) => state.dayTrack
+const mapStateToProps = (state) => state
 const mapDispatchToProps = actions
 
 export default connect(mapStateToProps, mapDispatchToProps)(DayTrackScreen)
